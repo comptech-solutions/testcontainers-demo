33279ff2d5b15a3c2836e4a46b511bd230fcf0d9
TheCloudfather <thecloudfather@gmail.com>
2023-01-19 23:56:49 -0600

    All of my changes to get things working

--- src/test/java/com/jetbrains/testcontainersdemo/CustomerIntegrationTests.java
+++ src/test/java/com/jetbrains/testcontainersdemo/CustomerIntegrationTests.java
@@ -3,16 +3,35 @@ package com.jetbrains.testcontainersdemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Testcontainers
 public class CustomerIntegrationTests {
 
     @Autowired
     private CustomerDao customerDao;
+
+    // Making a container static will stop a new database container from being created before each test method, creating only one
+    @Container
+    private static PostgreSQLContainer postgresContainer = new PostgreSQLContainer("postgres:14.4")
+            .withDatabaseName("employeesnet")
+            .withUsername("cmw")
+            .withPassword("dbtest");
+
+    @DynamicPropertySource
+    public static void overrideProps(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
+    }
 
     @Test
     void when_using_a_clean_db_this_should_be_empty() {
